e26add61d3db7c3f03041c5d6cfe09274e7afbcf
Annika Laumets <laumets.annika@gmail.com>
2015-03-01 19:59:23 +0200

    Main page update. New logos.

--- Webpage/js/fblogin.js
+++ Webpage/js/fblogin.js
@@ -1,6 +1,51 @@
 /**
  * Created by Ann on 01.03.2015.
  */
+
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        testAPI();
+        window.location.replace("http://timeraft.cloudapp.net/mainboard.php");
+    } else if (response.status === 'not_authorized') {
+        // The person is logged into Facebook, but not your app.
+        document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+        // The person is not logged into Facebook, so we're not sure if
+        // they are logged into this app or not.
+        document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+}
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+}
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '{your-app-id}',
+        cookie     : true,  // enable cookies to allow the server to access
+                            // the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v2.2' // use version 2.2
+    });
+
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+
+};
+
 (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
@@ -8,3 +53,13 @@
     js.src = "//connect.facebook.net/et_EE/sdk.js#xfbml=1&appId=426437317522257&version=v2.0";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
+    });
+}
--- Webpage/js/scripts.js
+++ Webpage/js/scripts.js
@@ -2,45 +2,22 @@
  * Created by Ann on 20.02.2015.
  */
 
-function signUp() {
-    document.getElementById("demo").innerHTML = "Sign up";
-}
-
-function signIn() {
-    document.getElementById("demo").innerHTML = "Sign in";
-}
-
 function div_show_signin() {
     document.getElementById("form_signin").style.display= "block";
+    document.getElementById("popup_signin").style.display = "block";
 }
 
 function div_show_signup() {
     document.getElementById("form_signup").style.display= "block";
+    document.getElementById("popup_signup").style.display = "block";
 }
 
 function div_hide_signin() {
     document.getElementById("form_signin").style.display = "none";
+    document.getElementById("popup_signin").style.display = "none";
 }
 
 function div_hide_signup() {
     document.getElementById("form_signup").style.display = "none";
-}
-
-function check_empty_signin() {
-    if (document.getElementById('name_signin').value == "" || document.getElementById('password_signin').value == "") {
-        alert("Fill All Fields!");
-    } else {
-        document.getElementById('form2_signin').submit();
-        alert("Form Submitted Successfully...");
-    }
-}
-
-function check_empty_signup() {
-    if (document.getElementById('name_signup').value == "" || document.getElementById('password_signup').value == "" ||
-        document.getElementById('email_signup').value == "") {
-        alert("Fill All Fields!");
-    } else {
-        document.getElementById('form2_signup').submit();
-        alert("Form Submitted Successfully...");
-    }
+    document.getElementById("popup_signup").style.display = "none";
 }
