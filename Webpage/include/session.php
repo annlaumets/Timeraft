1063934af1b49164ddb1b8abfb2e6b5da26a61b0
ViktoriaP <viktoria.plemakova@gmail.com>
2015-03-06 12:18:55 +0200

    Redirect to previous page after log in.

--- Webpage/include/session.php
+++ Webpage/include/session.php
@@ -7,5 +7,7 @@ $query = mysql_query("SELECT Email FROM Users WHERE Email = '$check_user'");
 $session = mysql_fetch_assoc($query)['Email'];
 
 if(!(isset($_SESSION['login'])) && !(isset($session))) {
-    header("location: /index.php");
+    header("Location: index.php?redirect=" . urlencode($_SERVER['REQUEST_URI']));
+    exit;
 }
+
