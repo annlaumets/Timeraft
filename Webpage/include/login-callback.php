b3184063ee04e9a66336d111cede6b8b07a42acf
Andre Tättar <b02166@ut.ee>
2015-03-22 18:43:54 +0200

    Fb login stuff

--- Webpage/include/login-callback.php
+++ Webpage/include/login-callback.php
@@ -17,15 +17,45 @@ session_start();
 
 require("db_connection.php");
 
-//file_put_contents('error.txt', $_POST, FILE_APPEND | LOCK_EX);
-
 
 if(!empty($_POST['data'])) {
+    // Define the data we are going to use here.
     $json = $_POST['data'];
     $array = json_decode($json, true); //tagastab jsoni array'na
-    $nimi = $array['first_name'];
+    $name = $array['first_name'];
     $email = $array['email'];
-    echo 'success';
+    $fbid = $array['id'];
+    $file = "testing.txt";
+    //Let's do something interesting now
+    $fbLogst = $conn->prepare("SELECT * FROM Users WHERE facebook_uid = :facebookid");
+    $fbLogst->execute(array('facebookid' => $fbid));
+    $result = $fbLogst->fetch(PDO::FETCH_ASSOC);
+
+    if (!empty($result)) {
+        // If not empty -> Login
+        file_put_contents($file, "Successful login by: ".$name, FILE_APPEND);
+        session_regenerate_id(true);
+        $_SESSION['login'] = true;
+        $_SESSION['loginUser'] = $email;
+        $updateLastVisited = $conn->prepare("Update Users SET Time_Last_Visited = now() WHERE facebook_uid = " . $fbid);
+        echo "Success";
+
+    } else {
+        // Empty -> Register
+        // Kutsub add_user meetodi, bindparam annab 3 parameetrit - name, pass, email
+        $insert = $conn->prepare("CALL sp_reg_fbemail('$name', '$fbid', '$email')");
+        $insert->execute();
+        if ($insert) {
+            session_regenerate_id(true);
+            $_SESSION['login'] = true;
+            $_SESSION['loginUser'] = $email;
+            header("location: /mainboard.php");
+        } else {
+            // Better error handling here ?
+            die("Signup error, Andre viga");
+        }
+        echo 'success';
+    }
 }
 else {
     die("Something went wrong.");
