7d45d74af627e3aa256476ab0994e68a9a0ae6f1
ViktoriaP <viktoria.plemakova@gmail.com>
2015-05-07 15:06:15 +0300

    Deleting boards and tasks

---
+++ Webpage/include/delete.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+require("db_connection.php");
+
+function delete_board($conn, $user_ID, $board_name) {
+    $delete = $conn->prepare("CALL sp_deleteBoard('$user_ID', '$board_name')");
+    $delete->execute();
+}
+
+function delete_task($conn, $user_ID, $taskID) {
+    $delete = $conn->prepare("CALL sp_deleteTask('$user_ID', '$taskID')");
+    $delete->execute();
+}
+
+$user_ID = $_SESSION['UserID'];
+
+if (isset($_POST['type'])) {
+    if ($_POST['type'] == 'board' && isset($_POST['name'])) {
+        $board_name = $_POST['name'];
+        delete_board($conn, $user_ID, $board_name);
+    } elseif ($_POST['type'] == 'task' && isset($_POST['name'])) {
+        $task_ID = $_POST['name'];
+        delete_task($conn, $user_ID, $task_ID);
+    }
+}
