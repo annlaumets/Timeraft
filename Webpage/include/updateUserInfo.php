9b7a87b6c70833dff24423a687e0ef2b1c37c032
Andre Tättar <b02166@ut.ee>
2015-05-24 20:08:50 +0300

    FileUpload functionality

--- Webpage/include/updateUserInfo.php
+++ Webpage/include/updateUserInfo.php
@@ -4,13 +4,69 @@ session_start();
 require("db_connection.php");
 include "login.php";
 
-function updateUserInfo($conn, $userID, $newName, $newPassword, $newBio) {
+function updateUserInfo($conn, $userID, $newName, $newPassword, $newBio, $fileLoc) {
     $delete = $conn->prepare("CALL sp_updateUserInfo(:userId, :newName, :newPw, :newBio)");
     $delete->execute(array('userId' => $userID, 'newName' => $newName, 'newPw' => $newPassword, 'newBio' => $newBio));
 }
 
+function uploadFile($file) {
+    // Praegu toetame ainult png faile. Lisaks määrame nimeks userid + .png
+    // Peab uurima, kas see turvaline või mitte -> Eeldus, et pigem on
+    file_put_contents("file.txt", $file["name"], FILE_APPEND);
+    $target_dir = "/var/www/uploads/";
+    $target_file = $target_dir . $_SESSION['UserID'].'png';
+    $uploadOk = 1;
+    $imageFileType = pathinfo($_FILES["fileToUpload"]["name"],PATHINFO_EXTENSION);
+    // Check if image file is a actual image or fake image
+    if(!empty($_FILES["uploaded_file"]) && ($_FILES['uploaded_file']['error'] == 0)) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        $check2 = is_image($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false && $check2 !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+
+    // Check file size - 1million Byte on siis limiit hetkel
+    if ($_FILES["fileToUpload"]["size"] > 1000000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Allow certain file formats, doublecheck, but its ok
+    if($imageFileType != "png") {
+        echo "Sorry, only PNG files are allowed.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            return $target_file;
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+}
+
+function is_image($path) {
+    // imageInfo[0] annab width ja imageInfo[1] annab height
+    $imageInfo = getimagesize($path);
+    $image_type = $imageInfo[2];
+    // Lisa siia veel tüüpe vajadusel - näiteks: IMAGETYPE_GIF , IMAGETYPE_JPEG ,IMAGETYPE_PNG , IMAGETYPE_BMP
+    if(in_array($image_type , array(IMAGETYPE_PNG))) {
+        return true;
+    }
+    return false;
+}
+
 if (isset($_POST)) {
-    $loginStmt = $conn->prepare("SELECT Password FROM Users WHERE ID = :uid");
+    $loginStmt = $conn->prepare("SELECT Password, Filepath FROM Users WHERE ID = :uid");
     $loginStmt->execute(array('uid' => $_SESSION['UserID']));
     $result = $loginStmt->fetch(PDO::FETCH_ASSOC);
 
@@ -25,7 +81,12 @@ if (isset($_POST)) {
             }
         }
         $user_ID = $_SESSION['UserID'];
-        updateUserInfo($conn, $user_ID, $_POST['nameChange'], $cpass, $_POST['bioChange']);
+        if (isset($_FILES)) {
+            $newFile = uploadFile($_FILES["fileToUpload"]);
+        } else {
+            $newFile = $result["Filepath"];
+        }
+        updateUserInfo($conn, $user_ID, $_POST['nameChange'], $cpass, $_POST['bioChange'], $newFile);
     } else {
         die("Old password was wrong");
     }
