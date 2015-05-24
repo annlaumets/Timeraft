b8a4aac192d8046132dd2361bb1ea0ae07918d3c
Andre Tättar <b02166@ut.ee>
2015-05-24 16:10:20 +0300

    Update user info backend, ("settings" page), some bug fixes

--- Webpage/include/db_connection.php
+++ Webpage/include/db_connection.php
@@ -6,5 +6,6 @@ try {
     $conn = new PDO('mysql:host=timeraft.cloudapp.net;dbname='.$iniArray["DBNAME"].';charset=utf8', $iniArray["DBUSER"], $iniArray["DBUSERPW"]);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
+    file_get_contents("dbError.txt",'ERROR: ' . $e->getMessage(),FILE_APPEND);
+    die("db connection error");
 }
